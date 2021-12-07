124fbaee4f385ac211ff33189972d4c766667054
Irina Lokosova <persik2466@gmail.com>
2021-12-07 20:27:20 +0400

    Добавьте файлы проекта.

---
+++ SF.Modul_14_3_3/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SF.Modul_14_3_3
+{
+    /// <summary>
+    /// Класс Контакт
+    /// </summary>
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ SF.Modul_14_3_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SF.Modul_14_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаём список с типом данных Contact
+            var phoneBook = new List<Contact>()
+            { 
+                // добавляем контакты
+                new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"),
+                new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"),
+                new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+                new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"),
+                new Contact("Сергей", "Брин", 799900000013, "serg@example.com"),
+                new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com")
+            };
+
+            //Сортировка сперва по имени, а затем по фамилии.
+            var sortphoneBook = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+
+            Console.WriteLine("Ведите номер страницы для вывода");
+
+            while (true)
+            {
+                // Читаем введенный с консоли символ
+                var input = Console.ReadKey().KeyChar;
+
+                // проверяем, число ли это
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                // если не соответствует критериям - показываем ошибку
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                // если соответствует - запускаем вывод
+                else
+                {
+                    // пропускаем нужное количество элементов и берем 2 для показа на странице
+                    var pageContent = sortphoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    // выводим результат
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
